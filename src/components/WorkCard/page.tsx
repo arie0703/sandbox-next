9edb86f6cfb95af5c380a385e634f5b6521d0515
K.A <kaiseiarimura0703@gmail.com>
2023-12-10 10:36:41 +0900

    Work一覧ページ作成

--- src/components/WorkCard/page.tsx
+++ src/components/WorkCard/page.tsx
@@ -1,17 +1,20 @@
 'use client'
 import styles from '@/styles/components/work-card.module.scss';
 import Image from 'next/image';
+import { Dispatch, SetStateAction } from 'react';
 
 interface WorkCardProps {
   title: string
   imagePath: string
+  setSelectedWorkIndex: (value: SetStateAction<number>) => void
+  workIndex: number
 }
 
-export const WorkCard = ({title, imagePath}: WorkCardProps) => {
+export const WorkCard = ({title, imagePath, setSelectedWorkIndex, workIndex}: WorkCardProps) => {
 
   return (
     <div className={styles['workCard']}>
-      <div className={styles['workCard__image']}>
+      <div className={styles['workCard__image']} onClick={() => {setSelectedWorkIndex(workIndex)}}>
         <Image src={imagePath} alt={title} width={200} height={200} />
       </div>
       <h3>{title}</h3>
---
+++ src/components/WorkDescription/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import styles from '@/styles/components/work-description.module.scss';
+
+interface WorkDescriptionProps {
+  title: string
+  description: string
+  workUrl: string
+  repositoryUrl: string
+}
+
+export const WorkDescription = ({title, description, workUrl, repositoryUrl}: WorkDescriptionProps) => {
+
+  return (
+    <div className={styles['workDescription']}>
+      <p className={styles['workDescription__title']}>
+        <a href={workUrl} target="_blank" rel="noopener noreferrer">{title}</a>
+      </p>
+      <div className={styles['workDescription__text']}>
+        <p>{description}</p>
+      </div>
+      <div className={styles['workDescription__repoLink']}>
+        <a href={workUrl} target="_blank" rel="noopener noreferrer">Repository</a>
+      </div>
+    </div>
+  )
+}
