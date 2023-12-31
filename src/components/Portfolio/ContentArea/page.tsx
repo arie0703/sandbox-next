485bc0cbfb58b4f9dd469b8c6691203167aebd8d
K.A <kaiseiarimura0703@gmail.com>
2023-12-31 09:26:31 +0900

    aboutのレイアウト変更

--- src/components/Portfolio/ContentArea/page.tsx
+++ src/components/Portfolio/ContentArea/page.tsx
@@ -4,29 +4,29 @@ import styles from '@/styles/components/Portfolio/content-area.module.scss';
 import { useState } from 'react';
 import { MenuBar } from '../MenuBar/page';
 import { WorkContainer } from '../WorkContainer/page';
-import { CareerContainer } from '../CareerContainer/page';
 import { ArticleContainer } from '../ArticleContainer/page';
 import { ArticleData } from '@/types/qiita';
+import { About } from '../About/page';
 
 interface ContentAreaProps {
   articles: ArticleData[]
 }
 
-export const ContentArea = ({articles}: ContentAreaProps) => {
+export const ContentArea = ({ articles }: ContentAreaProps) => {
 
-const [selectedItem, setSelectedItem] = useState('Top');
+  const [selectedItem, setSelectedItem] = useState('Top');
 
   return (
     <div className={styles.switchContent}>
-      <MenuBar setSelectedItem={setSelectedItem}/>
+      <MenuBar setSelectedItem={setSelectedItem} />
+      {
+        selectedItem === 'About' && <About />
+      }
       {
         selectedItem === 'Works' && <WorkContainer />
       }
       {
-        selectedItem === 'Career' && <CareerContainer />
-      }
-      {
-        selectedItem === 'Outputs' && <ArticleContainer articles={articles}/>
+        selectedItem === 'Outputs' && <ArticleContainer articles={articles} />
       }
     </div>
   )
